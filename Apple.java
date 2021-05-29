12bb8910d5a3f9ea937932a2f8b914dc4add035b
diz-56 <Diz-56@mail.ru>
2021-05-29 14:41:35 +0400

    hw-3

---
+++ Apple.java
@@ -0,0 +1,13 @@
+package surkova.geekbrains.java3.box;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
---
+++ Orange.java
@@ -0,0 +1,14 @@
+package surkova.geekbrains.java3.box;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+
+}
